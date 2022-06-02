2ea8a2697f6500f295e75660c7ecb59494cb29f1
ZekDaniels <zekavsrdev@gmail.com>
2022-06-02 11:04:08 +0300

    #29 feat: login added

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,6 +3,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
+
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
