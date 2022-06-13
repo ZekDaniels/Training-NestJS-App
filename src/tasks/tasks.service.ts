a6e8f42ace507fe9b70b8b03c2e48dbae28d3239
ZekDaniels <zekavsrdev@gmail.com>
2022-06-13 17:53:36 +0300

    45 fix: paths fixed in files

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,7 +5,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-task-dto';
 import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
