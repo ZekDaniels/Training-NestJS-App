fd58ddbd0de541012495209a6826e9e9ed339d7b
ZekDaniels <zekavsrdev@gmail.com>
2022-06-15 14:04:35 +0300

    #46 feat: task unit test created

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,41 @@
+import { Test } from '@nestjs/testing';
+import { TaskRepository } from './task.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someID',
+  password: 'somePass',
+  tasks: [],
+};
+
+describe('Tasks Service', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TaskRepository.getTasks and return the result', async () => {
+      expect(tasksRepository.getTasks).not.toHaveBeenCalled();
+
+      // call taskService.getTasks, which should then call repository's getTasks
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+    });
+  });
+});
